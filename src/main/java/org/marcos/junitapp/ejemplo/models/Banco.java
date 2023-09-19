04306bc163c473c81ecd5b2162ad4f55d36d14b0
marcos <martinmarcospablo@gmail.com>
2023-09-19 20:13:46 -0300

    Banco y cuentas

---
+++ src/main/java/org/marcos/junitapp/ejemplo/models/Banco.java
@@ -0,0 +1,35 @@
+package org.marcos.junitapp.ejemplo.models;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Banco {
+    private List<Cuenta> cuentas;
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Cuenta> getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(List<Cuenta> cuentas) {
+        this.cuentas = cuentas;
+    }
+
+    public void addCuenta(Cuenta cuenta) {
+        cuentas.add(cuenta);
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void transferir(Cuenta origen, Cuenta destino, BigDecimal monto){
+        origen.debito(monto);
+        destino.credito(monto);
+
+    }
+
+}
